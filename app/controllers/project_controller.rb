f40aa4e1ead85b20d24187f9617209690658d92b
Tilo Prütz <tilo@odin.(none)>
2009-02-21 23:56:54 +0100

    einfaches project create

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -2,4 +2,11 @@ class ProjectController < ApplicationController
   def index
     @projects = Project.find(:all)
   end
+
+  def create
+    p = Project.new(params[:project])
+    p.save
+    Branch.new(:name => 'master', :project_id => p.id).save
+    redirect_to :action => :index
+  end
 end
--- spec/controllers/project_controller_spec.rb
+++ spec/controllers/project_controller_spec.rb
@@ -1,9 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ProjectController, "when delivering index" do
-  before do
-  end
-
   it "should fetch all projects" do
     Project.should_receive(:find).with(:all).and_return []
     get 'index'
@@ -18,5 +15,24 @@ describe ProjectController, "when delivering index" do
   it "should render overview" do
     get 'index'
     response.should render_template('index')
+  end
+end
+
+describe ProjectController, "when creating a project" do
+  it "should create a project with the given name and url and a 'master' branch" do
+    project = mock_model(Project, :id => 666)
+    Project.should_receive(:new).with({"name" => "the name", "url" => "the url"}).and_return project
+    project.should_receive(:save)
+
+    branch = mock_model(Branch)
+    Branch.should_receive(:new).with({:name => "master", :project_id => 666}).and_return branch
+    branch.should_receive(:save)
+
+    post 'create', {:project => {:name => 'the name', :url => 'the url'}}
+  end
+
+  it "should redirect to index" do
+    post 'create', {:project => {:name => 'the name', :url => 'the url'}}
+    response.should redirect_to(:action => :index)
   end
 end
