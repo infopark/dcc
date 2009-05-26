a050bbf76b9da9f7b14d9b5c84734f0ff11b19e2
Tilo Pruetz <tilo@master-of-disaster.infopark>
2009-05-26 09:52:02 +0200

    Index-Rename entfernt
    Der zu erstellende neue Indexname ist in MySQL zu lang. Da der Index in Migration 4 sowieso gleich
    wieder gelöscht wird, wurden die Migrationsschritte 3 und 4 vereint.

--- db/migrate/003_create_builds.rb
+++ db/migrate/003_create_builds.rb
@@ -27,8 +27,8 @@ class CreateBuilds < ActiveRecord::Migration
     add_index :buckets, [:name, :build_id], :unique => true
     add_index :buckets, :name
     add_index :buckets, :build_id
-    remove_index :buckets, :column => [:project_id, :commit_hash]
-    remove_index :buckets, :column => [:name, :project_id, :commit_hash, :build_number]
+    remove_index :buckets, :column => [:project_id, :commit]
+    remove_index :buckets, :column => [:name, :project_id, :commit, :build_number]
     remove_index :buckets, :column => :build_number
     remove_index :buckets, :column => :project_id
     remove_columns :buckets, :commit_hash, :project_id, :build_number
@@ -48,8 +48,10 @@ class CreateBuilds < ActiveRecord::Migration
       bucket.save
     end
 
-    add_index :buckets, [:project_id, :commit_hash]
-    add_index :buckets, [:name, :project_id, :commit_hash, :build_number], :unique => true
+    add_index :buckets, [:project_id, :commit_hash],
+        :name => 'index_buckets_on_project_id_and_commit'
+    add_index :buckets, [:name, :project_id, :commit_hash, :build_number],
+        :name => 'index_buckets_on_name_and_project_id_and_commit_and_build_number', :unique => true
     add_index :buckets, :build_number
     add_index :buckets, :project_id
     remove_index :buckets, [:name, :build_id]
