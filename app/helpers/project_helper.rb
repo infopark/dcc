6b22c59dcf6865f8a82cf58cfa79fc88260bcd1f
Tilo Prütz <tilo@infopark.de>
2010-01-29 13:49:46 +0100

    progress indicator

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -3,14 +3,4 @@ module ProjectHelper
     Build.find_last_by_project_id_and_commit_hash(project.id, project.last_commit,
         :order => 'build_number')
   end
-
-  def project_display_status(project)
-    build = last_build(project)
-    build_display_status(build) if build
-  end
-
-  def project_status(project)
-    build = last_build(project)
-    build_status(build) if build
-  end
 end
--- spec/helpers/project_helper_spec.rb
+++ spec/helpers/project_helper_spec.rb
@@ -1,18 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ProjectHelper do
-  describe 'project_display_status' do
-    before do
-      @project = Project.new
-    end
-
-    it "should return the display value of the last build's status" do
-      helper.stub!(:last_build).with(@project).and_return('last build')
-      helper.stub!(:build_display_status).with('last build').and_return("last build's status")
-      helper.project_display_status(@project).should == "last build's status"
-    end
-  end
-
   describe "last_build" do
     before do
       @project = Project.new
@@ -24,18 +12,6 @@ describe ProjectHelper do
       Build.stub!(:find_last_by_project_id_and_commit_hash).with(666, 'der commit',
           :order => 'build_number').and_return('last build')
       helper.last_build(@project).should == 'last build'
-    end
-  end
-
-  describe "project_status" do
-    before do
-      @project = Project.new
-    end
-
-    it "should return the last build's status" do
-      helper.stub!(:last_build).with(@project).and_return('last build')
-      helper.stub!(:build_status).with('last build').and_return("last build's status")
-      helper.project_status(@project).should == "last build's status"
     end
   end
 end
