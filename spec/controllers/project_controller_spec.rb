2d1e46e9a33164d81c836fc8f3641bbbf40e0f09
Tilo Prütz <tilo@infopark.de>
2012-05-11 13:57:40 +0200

    [cleanup] Obsolete ProjectController-Specs

--- spec/controllers/project_controller_spec.rb
+++ spec/controllers/project_controller_spec.rb
@@ -1,12 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ProjectController, "when delivering index" do
-  it "should fetch all projects and assign them for the view" do
-    Project.should_receive(:find).with(:all).and_return "the projects"
-    get 'index'
-    assigns[:projects].should == "the projects"
-  end
-
   it "should render overview" do
     get 'index'
     response.should render_template('index')
@@ -33,29 +27,6 @@ describe ProjectController, "when deleting a project" do
   it "should remove the specified project" do
     Project.should_receive(:destroy).with("666")
     post 'delete', :id => 666
-  end
-
-  it "should redirect to index" do
-    Project.stub!(:destroy)
-    post 'delete', :id => 666
-    response.should redirect_to(:action => :index)
-  end
-end
-
-describe ProjectController, "when showing a project" do
-  before do
-    Project.stub!(:find).and_return nil
-  end
-
-  it "should fetch the project and assign it for the view" do
-    Project.should_receive(:find).with("666").and_return "the project"
-    get 'show', :id => 666
-    assigns[:project].should == "the project"
-  end
-
-  it "should render the project view" do
-    get 'show', :id => 666
-    response.should render_template('project/show')
   end
 end
 
@@ -94,19 +65,10 @@ describe ProjectController, "when showing a bucket" do
 end
 
 describe ProjectController, "when requesting a project to build" do
-  before do
-    Project.stub!(:find).and_return mock('project', :build_requested= => nil, :save => nil)
-  end
-
   it "should set the build flag and save the project" do
-    Project.should_receive(:find).with("666").and_return(project = mock('project'))
+    Project.should_receive(:find).with("666").and_return(project = mock('project', :to_json => nil))
     project.should_receive(:build_requested=).with(true).ordered
     project.should_receive(:save).ordered
     post 'build', :id => 666
-  end
-
-  it "should redirect to index" do
-    post 'build', :id => 666
-    response.should redirect_to(:action => :index)
   end
 end
