ecf1bd79af130dc330513eb145bce70e6ef920fd
Tilo Prütz <tilo@infopark.de>
2012-05-16 11:27:09 +0200

    [cruise] beim Cruise wird rerun.txt ignoriert

--- dcc_config.rb
+++ dcc_config.rb
@@ -4,5 +4,5 @@ before_all.performs_rake_tasks("db:migrate")
 
 buckets "test" do
   bucket(:specs).performs_rake_tasks("spec")
-  bucket(:features).performs_rake_tasks("cucumber")
+  bucket(:features).performs_rake_tasks("cucumber:full_ok")
 end
