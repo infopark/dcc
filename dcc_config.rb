84f4c343e4ef0633f70127fe125ea0369df4e793
Tilo Prütz <tilo@infopark.de>
2012-06-22 10:22:10 +0200

    database.yml wird durch Cruise-Setup erzeugt

--- dcc_config.rb
+++ dcc_config.rb
@@ -1,4 +1,25 @@
+require 'yaml'
+
 send_notifications_to "tilo@infopark.de"
+
+before_all do
+  File.open("config/database.yml", "w") do |f|
+    f.puts({
+      'test' => {
+        'adapter' => 'sqlite3',
+        'database' => 'db/test.sqlite3',
+        'pool' => 5,
+        'timeout' => 5000
+      },
+      'cucumber' => {
+        'adapter' => 'sqlite3',
+        'database' => 'db/test.sqlite3',
+        'pool' => 5,
+        'timeout' => 5000
+      }
+    }.to_yaml)
+  end
+end
 
 before_all.performs_rake_tasks("db:migrate")
 
