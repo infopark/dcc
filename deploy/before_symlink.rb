336b8d53f0e8c0b120d134a55bdf70fa2bd17a4c
Tilo Prütz <tilo@infopark.de>
2013-11-27 21:43:52 +0100

    [gui][deploy] Rails-Env von DCC statt von Employeeapps

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -4,7 +4,7 @@ Chef::Log.info('= Precompile assets')
 execute 'rake assets:precompile' do
   cwd release_path
   command 'bundle exec rake assets:precompile'
-  environment 'RAILS_ENV' => node[:deploy][:employeeapps][:rails_env]
+  environment 'RAILS_ENV' => node[:deploy][:dcc][:rails_env]
 end
 
 Chef::Log.info('= Write revision.txt')
