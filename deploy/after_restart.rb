a575296d70b52492819daf422e7f16b81342dd19
Tilo Prütz <tilo@infopark.de>
2013-11-27 21:43:19 +0100

    [gui][deploy] Repo für Honeybadger-Deploy aus Cloud-Config

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,4 +2,5 @@ Chef::Log.info('=== Hook: after_restart')
 
 Chef::Log.info('= Notify honeybadger about deploy')
 revision = %x(git rev-parse HEAD).strip
-run "bundle exec rake environment honeybadger:deploy_with_environment TO=production REVISION=#{revision} REPO=https://github.com/infopark/dcc"
+run "bundle exec rake environment honeybadger:deploy_with_environment TO=production " +
+    "REVISION=#{revision} REPO=#{node[:deploy][:dcc][:scm][:repository]}"
