81f1ab5a3cc4ebfaef466b103b2a19b3eca34a2e
Tilo Prütz <tilo@infopark.de>
2014-05-09 11:06:38 +0200

    [rails4] secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dcc::Application.config.secret_token = 'ec6e3c65b511f234546228ab95c2bd648a33a65255b69ed1d4001df5d8c34644b1732b93d99d93a5c519b3523b9080b6af39da28e6ed44e51208c4356dd550e5'
+Dcc::Application.config.secret_key_base = 'OwaFIu7A3r38XobJVRsjr3HAr6Y00O2RhzUphfPnLwQOlVNjZBS1fPAddKawG9Ty7la8bC4uPDI4dzsbJWgZJzbwRlzEgmfUd9LJAZAIL1X6UzBQsBD0aMW82VtVN6Vy'
